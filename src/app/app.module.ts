1df8da146b39c19ea804d7164e2f0058dbfce529
Aziz <aziz.aroug.9@gmail.com>
2024-02-03 09:56:04 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './MainPage/main-page/main-page.component';
+import { NavBarComponent } from './MainPage/nav-bar/nav-bar.component';
+import { FooterComponent } from './MainPage/footer/footer.component';
+import { RecommandationComponent } from './MainPage/recommandation/recommandation.component';
+import { WelcomePageComponent } from './MainPage/welcome-page/welcome-page.component';
+import { BenefitsComponent } from './MainPage/benefits/benefits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavBarComponent,
+    FooterComponent,
+    RecommandationComponent,
+    WelcomePageComponent,
+    BenefitsComponent
   ],
   imports: [
     BrowserModule,
